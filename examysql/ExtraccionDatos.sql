8f781893791ad972b93cd2074c271724b4cc9b7c
joseing <jjosecamaluque@gmail.com>
2020-05-28 13:07:15 -0500

    prueb

---
+++ examysql/ExtraccionDatos.sql
@@ -0,0 +1,8 @@
+SELECT  
+	p.Cod_prod, CONCAT(p.Nom_prod, ' ', p.Concent, ' ', p.Presentac, ' frac', p.Fracciones ) as Nom_prod, 
+    p.Prec_compra, p.Prec_venta,
+	c.Nom_cat,
+	f.Nom_fam
+FROM farmadb.producto as p  
+	inner join farmadb.categoria as c on p.Cat_id= c.Cat_id 
+    inner join farmadb.familia as f on c.Fam_id= f.Fam_id;
